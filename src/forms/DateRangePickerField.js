49686d50a88e565fd44351d88de527cfbb950e4d
Lucas Lira Gomes <llira@kayak.com>
2018-12-10 15:36:43 +0100

    Get rid of the boostrap date range picker. This will inderectly remove the need for jquery.

--- src/forms/DateRangePickerField.js
+++ src/forms/DateRangePickerField.js
@@ -3,21 +3,23 @@ import _ from 'lodash';
 import PropTypes from 'prop-types';
 import FormField from './FormField';
 import { DateRangePicker } from '../ui';
-import { DEFAULT_DATE_FORMAT, getDatePickerRanges } from '../ui/DateRangePicker';
+import {
+  DEFAULT_DATE_FORMAT,
+  getDatePickerInputRanges,
+  getDatePickerRanges,
+  VISIBLE_RANGES,
+} from '../ui/DateRangePicker';
 
 /**
  * Component that encapsulates the Date Range Picker component as Redux Form field component.
  */
 const DateRangePickerField = ({
-  input, label, help, meta, minDate, maxDate, dateFormat, ranges, onChangeValue,
+  input, label, help, meta, onChangeValue, ...props
 }) => (
   <FormField id={input.id} label={label} help={help} meta={meta}>
     <DateRangePicker
+      {...props}
       value={input.value || undefined}
-      minDate={minDate}
-      maxDate={maxDate}
-      dateFormat={dateFormat}
-      ranges={ranges}
       onChange={(params) => {
         input.onChange(params);
         if (onChangeValue) onChangeValue(params);
@@ -40,22 +42,46 @@ DateRangePickerField.propTypes = {
    */
   help: PropTypes.string,
   /**
-   * A minimum date.
+   * A minimum date. Anything prior to it will be disabled.
    */
   minDate: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   /**
-   * A maximum date.
+   * A maximum date. Anything after it will be disabled.
    */
   maxDate: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   /**
-   * A date format as a string.
+   * A date format as a string. Check out date-fns's format option for syntax.
    */
   dateFormat: PropTypes.string,
   /**
    * An object with labels as keys and tuples shaped as [startDate, endDate] as values. Those will be used as
    * the window ranges that will be available for selection. See getDatePickerRanges function for more info.
+   * For disabling ranges, set this to empty list.
    */
   ranges: PropTypes.objectOf(PropTypes.array),
+  /**
+   * An object with labels as keys and an object shaped as {interval, endDate} as values. Those will be used as
+   * the input ranges that will be available for selection. See getDatePickerInputRanges function for more info.
+   * For disabling input ranges, set this to empty list.
+   */
+  inputRanges: PropTypes.objectOf(PropTypes.object),
+  /**
+   * Sets the direction of the calendar overlay.
+   */
+  placement: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
+  /**
+   * The amount of calendar months that will be rendered.
+   */
+  numCalendarMonths: PropTypes.number,
+  /**
+   * The direction of calendar months. This is only relevant when you have more than one as numCalendarMonths.
+   */
+  calendarDirection: PropTypes.oneOf(['vertical', 'horizontal']),
+  /**
+   * A list of dates that will be disabled for selection. For the purpose of specifying a valid selection interval,
+   * use minDate and maxDate props instead.
+   */
+  disabledDates: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.string])),
   /**
    * Metadata object that is passed by the React Form
    */
@@ -78,7 +104,12 @@ DateRangePickerField.defaultProps = {
   minDate: null,
   maxDate: null,
   dateFormat: DEFAULT_DATE_FORMAT,
-  ranges: getDatePickerRanges(),
+  ranges: _.pickBy(getDatePickerRanges(), (value, key) => VISIBLE_RANGES.includes(key)),
+  inputRanges: getDatePickerInputRanges(),
+  placement: 'bottom',
+  numCalendarMonths: 1,
+  calendarDirection: 'vertical',
+  disabledDates: [],
   meta: { touched: false, error: [] },
   onChangeValue: _.noop,
 };
